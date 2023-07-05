ae7ad95f467fe9531072b8fdd5116d08c31ed31f
RMBapi <rafidmhotasimbapi@gmail.com>
2023-07-06 01:12:45 +0600

    hello

--- config/constants.php
+++ config/constants.php
@@ -9,7 +9,7 @@
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'onlinefoodorder');
-    // Hello bye
+   
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
